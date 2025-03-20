5d7c6e95687657ff2114672b715404acf3735f51
SergioAle210 <ore221122@uvg.edu.gt>
2025-03-20 00:33:10 -0600

    FEAT: Se definieron las funciones y constantes necesarias para el cliente.

---
+++ src/client/client.h
@@ -0,0 +1,18 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <libwebsockets.h>
+
+// Obtiene el timestamp actual en formato ISO 8601 y lo guarda en el buffer.
+void get_timestamp(char *buffer, size_t size);
+
+// Funciones para enviar mensajes en formato JSON a través del WebSocket.
+int send_register_message(struct lws *wsi, const char *username);
+int send_broadcast_message(struct lws *wsi, const char *username, const char *message);
+int send_private_message(struct lws *wsi, const char *username, const char *target, const char *message);
+int send_list_users_message(struct lws *wsi, const char *username);
+int send_user_info_message(struct lws *wsi, const char *username, const char *target);
+int send_change_status_message(struct lws *wsi, const char *username, const char *status);
+int send_disconnect_message(struct lws *wsi, const char *username);
+
+#endif // CLIENT_H
