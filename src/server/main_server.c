223e80b75dc5a94b17c816281a5b40454cf0aa1e
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2025-03-21 04:10:50 -0600

    feat: update main

--- src/server/main_server.c
+++ src/server/main_server.c
@@ -1,10 +1,14 @@
 #include "server.h"
+#include <pthread.h>
 
 static int callback_chat(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
     switch (reason) {
         case LWS_CALLBACK_ESTABLISHED: {
             printf("Cliente conectado\n");
-            // Envía mensaje de bienvenida
+            char client_ip[48] = {0};
+            lws_get_peer_simple(wsi, client_ip, sizeof(client_ip));
+            printf("IP del cliente: %s\n", client_ip);
+            // Envía mensaje de bienvenida, etc.
             const char *msg = "{\"type\": \"server\", \"content\": \"Conexión establecida\"}";
             size_t msg_len = strlen(msg);
             unsigned char buf[LWS_PRE + msg_len];
@@ -12,7 +16,7 @@ static int callback_chat(struct lws *wsi, enum lws_callback_reasons reason, void
             strcpy((char *)p, msg);
             lws_write(wsi, p, msg_len, LWS_WRITE_TEXT);
             break;
-        }
+        }        
         case LWS_CALLBACK_RECEIVE:
             handle_message((char *)in, wsi);
             break;
@@ -43,11 +47,23 @@ static struct lws_protocols protocols[] = {
     { NULL, NULL, 0, 0 }
 };
 
-int main() {
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <puerto>\n", argv[0]);
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+    if (port <= 0) {
+        fprintf(stderr, "Puerto inválido.\n");
+        return 1;
+    }
+
     struct lws_context_creation_info info = {0};
     struct lws_context *context;
     
-    info.port = 8080;
+    info.port = port;
     info.protocols = protocols;
     context = lws_create_context(&info);
     if (!context) {
@@ -55,11 +71,13 @@ int main() {
         return -1;
     }
     
-    printf("Servidor WebSocket en puerto 8080\n");
+    printf("Servidor WebSocket en puerto %d\n", port);
+
+
     while (1) {
         lws_service(context, 50);
     }
-    
+
     lws_context_destroy(context);
     return 0;
 }
